f95d516541b249fc7caffad2bf649b1fd4944c8b
Yechan0903 <oolo9876@naver.com>
2023-11-15 23:18:50 +0900

    day1

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
